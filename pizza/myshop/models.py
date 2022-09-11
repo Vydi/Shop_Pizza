52066c832cf8b3ccb885fe8b96a554647d9f5e42
name <name@gmail.com>
2022-09-11 11:28:58 +0300

    first

---
+++ pizza/myshop/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, db_index=True)
+    slug = models.CharField(max_length=150, db_index=True, unique=True)
+    image = models.ImageField(upload_to='product/%Y/%m/%d', blank=True)
+    description = models.TextField(max_length=1000, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    uploaded = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('myshop:product_detail', args=[self.id, self.slug])
